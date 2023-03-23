1e4480ee3ea2ebb2fae5d60224b1f06d3b80e7b5
avidit <aabishkar@gmail.com>
2023-03-22 20:15:10 -0400

    Add board object

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,93 +2,102 @@
   import { onMount } from 'svelte';
 
   let canvas;
+  let ctx;
+
   export let canvasWidth = 500;
   export let canvasHeight = 500;
 
   let charWidth = 50;
   let charHeight = 50;
 
-  let a = [canvasWidth / 2, 50];
-  let b = [canvasWidth / 2 - 75, 150];
-  let c = [canvasWidth / 2, 150];
-  let d = [canvasWidth / 2 + 75, 150];
-  let e = [canvasWidth / 2 - 150, canvasHeight / 2];
-  let f = [canvasWidth / 2, 250];
-  let g = [canvasWidth / 2 + 150, canvasHeight / 2];
-  let h = [canvasWidth / 2 - 150, 400];
-  let i = [canvasWidth / 2, 400];
-  let j = [canvasWidth / 2 + 150, 400];
-
-  let turn = 'Goat';
-  let playAs;
+  let board = {
+    a: { coord: [canvasWidth / 2, 50], paths: ['b', 'c', 'd'], jumpPaths: ['e', 'g'] },
+    b: { coord: [canvasWidth / 2 - 75, 150], paths: ['a', 'c', 'e'], jumpPaths: [] },
+    c: { coord: [canvasWidth / 2, 150], paths: ['a', 'b', 'd'], jumpPaths: [] },
+    d: { coord: [canvasWidth / 2 + 75, 150], paths: ['a', 'c', 'g'], jumpPaths: [] },
+    e: { coord: [canvasWidth / 2 - 150, canvasHeight / 2], paths: ['b', 'f'], jumpPaths: ['g'] },
+    f: { coord: [canvasWidth / 2, 250], paths: ['e', 'g', 'i'], jumpPaths: [] },
+    g: { coord: [canvasWidth / 2 + 150, canvasHeight / 2], paths: ['d', 'f'], jumpPaths: ['e'] },
+    h: { coord: [canvasWidth / 2 - 150, 400], paths: ['i'], jumpPaths: ['j'] },
+    i: { coord: [canvasWidth / 2, 400], paths: ['f', 'h', 'j'], jumpPaths: [] },
+    j: { coord: [canvasWidth / 2 + 150, 400], paths: ['i'], jumpPaths: ['h'] }
+  };
 
   onMount(() => {
-    const ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d');
     drawBoard(ctx);
-    updateBoard(ctx, a, [h, i, j]);
-    highightPositon(ctx, i);
+    newGame();
   });
 
   const drawLine = (ctx, start, end) => {
     ctx.beginPath();
-    ctx.moveTo(...start);
-    ctx.lineTo(...end);
+    ctx.moveTo(...start.coord);
+    ctx.lineTo(...end.coord);
     ctx.stroke();
   };
 
-  const drawCharacter = (ctx, src, position) => {
-    let [x, y] = position;
+  const drawCharacter = (ctx, position) => {
+    let [x, y] = position.coord;
     let image = new Image();
-    image.src = src;
+    image.src = `${position.char}.png`;
     image.onload = () =>
       ctx.drawImage(image, x - charWidth / 2, y - charHeight / 2, charWidth, charHeight);
   };
 
-  const drawTiger = (ctx, position) => drawCharacter(ctx, 'tiger.png', position);
-  const drawGoat = (ctx, position) => drawCharacter(ctx, 'goat.png', position);
-
   const drawBoard = (ctx) => {
     const options = { lineCap: 'round', lineJoin: 'round', lineWidth: 3 };
     Object.assign(ctx, options);
-    drawLine(ctx, a, e);
-    drawLine(ctx, a, c);
-    drawLine(ctx, a, g);
-    drawLine(ctx, b, d);
-    drawLine(ctx, e, g);
-    drawLine(ctx, f, i);
-    drawLine(ctx, h, j);
+    drawLine(ctx, board.a, board.e);
+    drawLine(ctx, board.a, board.c);
+    drawLine(ctx, board.a, board.g);
+    drawLine(ctx, board.b, board.d);
+    drawLine(ctx, board.e, board.g);
+    drawLine(ctx, board.f, board.i);
+    drawLine(ctx, board.h, board.j);
   };
 
-  const updateBoard = (ctx, tiger = a, goats = [h, i, j]) => {
+  const updateBoard = (ctx, board) => {
     drawBoard(ctx);
-    drawTiger(ctx, tiger);
-    goats.forEach((goat) => drawGoat(ctx, goat));
+    for (const [_, v] of Object.entries(board)) {
+      if (v.char) {
+        drawCharacter(ctx, v);
+      }
+    }
   };
 
   const highightPositon = (ctx, position) => {
-    const options = { lineWidth: 2, strokeStyle: '#16a34a' }
+    const options = { lineWidth: 2, strokeStyle: '#16a34a' };
     Object.assign(ctx, options);
-    let [x, y] = position;
+    let [x, y] = position.coord;
     ctx.beginPath();
     ctx.rect(x - charWidth / 2, y - charHeight / 2, charWidth, charHeight);
     ctx.stroke();
   };
 
-  const moveChar = (ctx, to) => {
-    ctx.clearRect(0, 0, canvas.canvasWidth, canvas.height);
-    drawBoard(ctx, to);
+  const moveChar = (ctx, c, from, to) => {
+    clearBoard();
+    board[from].char = '';
+    board[to].char = c;
+    updateBoard(ctx, board);
   };
 
-  const isEmpty = (ctx, position) => {
-    return true;
+  const newGame = () => {
+    clearBoard();
+    board.a.char = 'tiger';
+    board.b.char = '';
+    board.c.char = '';
+    board.d.char = '';
+    board.e.char = '';
+    board.f.char = '';
+    board.g.char = '';
+    board.h.char = 'goat';
+    board.i.char = 'goat';
+    board.j.char = 'goat';
+    updateBoard(ctx, board);
   };
 
-  const canMove = (ctx, from, to) => {
-    isEmpty(to) ? true : false;
-  };
-
-  const canCapture = (ctx, from, to) => {
-    return true;
+  const clearBoard = () => {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
   };
 </script>
 
@@ -113,7 +122,7 @@
             name="playAs"
             id="playAsTiger"
           />
-          <label class="form-check-label text-gray-800" for="playAsTiger"> Tiger </label>
+          <label class="form-check-label text-gray-800" for="playAsTiger">Tiger</label>
         </div>
         <div class="form-check">
           <input
@@ -123,15 +132,15 @@
             id="playAsGoat"
             checked
           />
-          <label class="form-check-label inline-block text-gray-800" for="playAsGoat"> Goat </label>
+          <label class="form-check-label inline-block text-gray-800" for="playAsGoat">Goat</label>
         </div>
       </div>
 
-      <button class="m-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded">
-        New Game
-      </button>
-
-      <p class="m-5 text-lg text-center text-gray-800 font-bold">Turn: {turn}</p>
+      <button
+        on:click={newGame}
+        class="m-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
+        >New Game</button
+      >
     </div>
   </div>
 </div>
